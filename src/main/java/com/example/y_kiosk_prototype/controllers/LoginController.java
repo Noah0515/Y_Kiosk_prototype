94d544a2ab5851aa5617d4793b36ef8f27ec0e79
Yoon <01csyoon@gmail.com>
2025-12-20 03:42:06 +0900

    fix: 반환 페이지 수정
    로그도 추가

--- src/main/java/com/example/y_kiosk_prototype/controllers/LoginController.java
+++ src/main/java/com/example/y_kiosk_prototype/controllers/LoginController.java
@@ -1,14 +1,20 @@
 package com.example.y_kiosk_prototype.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
 public class LoginController {
+
+
     @GetMapping("/login/oauth2/kakao")
+    @PreAuthorize("hasAnyRole('NORMAL')")
     public String kakaoLoginRedirect(){
-        return "/login-redirect";
+        log.info("kakaoLoginRedirect");
+
+        return "login/login-redirect";
     }
 }
