4e9f79bf3b02ac4b99c9497d5fac9e06d6eeee16
Yoon <01csyoon@gmail.com>
2025-12-20 03:24:34 +0900

    feat: 가게 관리관련 기본 컨트롤러, 페이지
    별 기능없이 페이지 반환만 하는정도만 해둠.

---
+++ src/main/java/com/example/y_kiosk_prototype/controllers/StoreController.java
@@ -0,0 +1,13 @@
+package com.example.y_kiosk_prototype.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StoreController {
+
+    @GetMapping("/store")
+    public String getStorePage() {
+        return "store";
+    }
+}
