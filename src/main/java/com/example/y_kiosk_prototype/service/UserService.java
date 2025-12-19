a555d46da5e5a20a0cc3ff9baafbbac57d050c37
Yoon <01csyoon@gmail.com>
2025-12-20 05:01:14 +0900

    feat: userid 생성 메소드 추가
    숫자,알파벳 대소문자로 이루어진 60자리 문자열

--- src/main/java/com/example/y_kiosk_prototype/service/UserService.java
+++ src/main/java/com/example/y_kiosk_prototype/service/UserService.java
@@ -5,8 +5,14 @@ import com.example.y_kiosk_prototype.entity.UserInfo;
 import com.example.y_kiosk_prototype.repository.SocialLoginRepository;
 import com.example.y_kiosk_prototype.repository.UserAccountRepository;
 import com.example.y_kiosk_prototype.repository.UserInfoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Service
 public class UserService {
     private final SocialLoginRepository socialLoginRepository;
@@ -20,15 +26,29 @@ public class UserService {
     }
 
     public void addKakaoAccount(UserInfo userInfo, SocialLogin socialLogin){
+        log.info("addKakaoAccount");
         userInfoRepository.save(userInfo);
+        log.info("saved userInfo: " + userInfo.toString());
         socialLoginRepository.save(socialLogin);
+        log.info("saved SocialLogin: " + socialLogin.toString());
     }
 
     public UserInfo findUserByUserId(String userId){
         return userInfoRepository.findByUserId(userId).get();
     }
 
-    public SocialLogin findSocialLoginByKakaoId(String kakaoId){
-        return socialLoginRepository.findByKakaoId(kakaoId).get();
+    public Optional<SocialLogin> findSocialLoginByKakaoId(String kakaoId){
+        return socialLoginRepository.findByKakaoId(kakaoId);
+    }
+
+    public String getNewUserId() {
+        String characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        String result = new Random().ints(60, 0, characters.length())
+                .mapToObj(characters::charAt)
+                .map(Object::toString)
+                .collect(Collectors.joining());
+
+        return result;
     }
 }
