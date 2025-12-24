2cf2d692435ebb17081eda48b6a171907791e78b
Yoon <01csyoon@gmail.com>
2025-12-25 04:09:57 +0900

    fix: 반환값 Store로 변경

--- src/main/java/com/example/y_kiosk_prototype/service/StoreService.java
+++ src/main/java/com/example/y_kiosk_prototype/service/StoreService.java
@@ -5,18 +5,22 @@ import com.example.y_kiosk_prototype.entity.Store;
 import com.example.y_kiosk_prototype.entity.UserInfo;
 import com.example.y_kiosk_prototype.repository.StoreRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class StoreService {
     private final StoreRepository storeRepository;
 
-    public void createStore(@RequestBody StoreReqDto storeReqDto, UserInfo userInfo) {
+    public Store createStore(@RequestBody StoreReqDto storeReqDto, UserInfo userInfo) {
         Store store = storeReqDto.toEntity(userInfo);
 
         storeRepository.save(store);
+        log.info("Store {} created", store.getStoreId());
 
+        return store;
     }
 }
