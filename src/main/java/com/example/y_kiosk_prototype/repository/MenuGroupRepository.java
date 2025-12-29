e4afbc8150e2b0bf044ef60c6705f0ac5b050bac
Yoon <01csyoon@gmail.com>
2025-12-30 02:10:42 +0900

    feat: 메뉴그룹 아이디로 메뉴그룹 찾기

--- src/main/java/com/example/y_kiosk_prototype/repository/MenuGroupRepository.java
+++ src/main/java/com/example/y_kiosk_prototype/repository/MenuGroupRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MenuGroupRepository extends JpaRepository<MenuGroup, Integer> {
     List<MenuGroup> findAllByStore(Store store);
+
+    Optional<MenuGroup> findMenuGroupByMenuGroupId(int menuGroupId);
 }
