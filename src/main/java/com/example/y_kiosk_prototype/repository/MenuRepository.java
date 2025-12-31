3c0691bf589fcc86129f38d45c410dc929b9b9f5
Yoon <01csyoon@gmail.com>
2025-12-31 11:04:47 +0900

    feat: 메뉴 찾기

--- src/main/java/com/example/y_kiosk_prototype/repository/MenuRepository.java
+++ src/main/java/com/example/y_kiosk_prototype/repository/MenuRepository.java
@@ -4,7 +4,10 @@ import com.example.y_kiosk_prototype.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Integer> {
+    Optional<Menu> findMenuByMenuId(int menuId);
 
 }
